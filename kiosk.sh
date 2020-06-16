d9174f2905d82c0b476d8252ce0b8a9ddd5051b4
Wim Tobback <wim.tobback@gmail.com>
2020-06-16 13:08:22 +0200

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+printf "Start kiosk\n"
+
+input="$HOME/kiosk.conf"
+sites=""
+while IFS= read -r var
+do
+  sites=$sites" "$var
+done < "$input"
+
 xset s noblank
 xset s off
 xset -dpms
@@ -8,14 +17,7 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-input="$HOME/kiosk.conf"
-sites=""
-while IFS= read -r var
-do
-  sites=$sites" "$var
-done < "$input"
-
-/usr/bin/chromium-browser --disable-component-update --check-for-update-interval=31536000 --enable-features=OverlayScrollbar --disable-restore-session-stat$
+/usr/bin/chromium-browser --disable-component-update --check-for-update-interval=31536000 --enable-features=OverlayScrollbar --disable-restore-session-state --noerrdialogs --disable-infobars --kiosk $sites &
 
 while true; do
    xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
